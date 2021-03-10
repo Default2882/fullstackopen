a518de12200fb703d20bf1b3b5982c5ac12b3df0
Default2882 <soumyasaurav@gmail.com>
2021-03-10 13:23:45 +0530

    Changed the directory structure a bit

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react'
 
-const App = () =>  {
+
+const Cock = (props) => {
+    console.log(props.name)
+    return (
     <div>
-        <p>Hello World</p>
+        <p>bruh {props.name}</p>
     </div>
+    )
 }
 
+const App = () => {
+    const now = new Date();
+    const a = 10;
+    const b = 20;
 
-export default App;
+    return (
+    <>
+        <p> Hello world, it is now {now.toString()} </p>
+        <p> I hate CBT!! HAHA!! </p>
+        <p> {a} plus {b} is {a+b} </p>
+        <Cock name="Deez"/>
+        <Cock name="nutss"/>
+        <Cock name="ecsdee"/>
+        <Cock name="lololololol"/>
+    </>
+    );
+}
+
+export default App
